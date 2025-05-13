5053fd38d812c855a878e95364d50bb408c0e69d
Nicholas Denholm <nicholas.denholm1@gmail.com>
2025-05-13 18:34:10 -0400

    modified:   .gitignore 	new file:   activate.sh 	new file:   gitadd.sh

---
+++ activate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=$(pwd)
+
+# Navigate to the directory where the virtual environment is located
+#cd /python_projects/pigeon_turret
+echo $DIR
+cd $DIR
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# You can optionally run other commands here, for example, running a script
+# python your_script.py
+
